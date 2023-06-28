a8de990719ab4f596ade8a6e83d0f28cef3807ac
miusarname <oscarm.alvarezg@gmail.com>
2023-06-28 17:25:22 -0500

    feat: :sparkles: Se reinicializa el front-end
    Se reinicializa el front-end por inconvenientes en el desarrollo del mismo

--- front-end/app.js
+++
@@ -1,9 +0,0 @@
-const btnSelect = document.querySelectorAll(".selectedE");
-const sidebars = document.querySelector('#sidebar')
-var toInsert;
-
-btnSelect.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    console.log(btn.textContent);
-  });
-});
